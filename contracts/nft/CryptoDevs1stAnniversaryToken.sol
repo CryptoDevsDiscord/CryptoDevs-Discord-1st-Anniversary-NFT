d6a6d4f4a6763267d55215f04273937264b1d9ce
Luiserebii <luis@serebii.io>
2019-07-29 21:57:19 -0400

    Adding NFT contract file

---
+++ contracts/nft/CryptoDevs1stAnniversaryToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Enumerable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
+
+
+contract CryptoDevs1stAnnversaryToken {
+
+
+
+}
