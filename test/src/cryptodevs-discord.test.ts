086110edcc79c46cf2dfea043e39ae954f5065f3
Luiserebii <luis@serebii.io>
2019-08-01 05:28:27 -0400

    Setting up tests

---
+++ test/src/cryptodevs-discord.test.ts
@@ -0,0 +1,19 @@
+/**
+
+==========================================
+   Testing CryptoDevs1stAnnversaryToken
+==========================================
+
+*/
+
+const CryptoDevs1stAnnversaryToken = artifacts.require('CryptoDevs1stAnnversaryToken');
+
+let instances = {};
+
+contract('CryptoDevs1stAnnversaryToken', (accounts) => {
+
+  it('meme', async () => {
+
+  })
+
+})
